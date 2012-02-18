3b368d88222f5da7c3cb7130a3512b90f13df8cf
Mac <mahmood1@Mahmood1s-Mac.local>
2012-02-18 19:25:28 +0100

    Initial Commit

---
+++ flip horizontal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  flip horizontal
+//
+//  Created by Жека Г on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ flip horizontal/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  flip horizontal
+//
+//  Created by Жека Г on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
