89728e7337d7e28c57cd94aa54bd027e1a3e024a
Eugene Hyrol <jekahy@gmail.com>
2012-02-22 22:09:04 +0100

    fg
    erg

--- flip horizontal/AppDelegate.m
+++ flip horizontal/AppDelegate.m
@@ -17,7 +17,7 @@
     NSString *path=@"/Users/mahmood1/Desktop/image1.jpg"; 
     NSImage *image=[[NSImage alloc]initWithContentsOfFile:path ];
     [view setImage:image];
-    //[view vi]
+    
 
 
 
@@ -26,25 +26,20 @@
     NSString *path=@"/Users/mahmood1/Desktop/image1.jpg"; 
     NSImage *image=[[NSImage alloc]initWithContentsOfFile:path ];
     NSSize size=[image size];
-    //NSPoint center=NSMakePoint(size.width/2.0, size.height/2.0);
     NSImage *rotatedimage;
     NSAffineTransform *t=[NSAffineTransform transform];
-    //NSSize size=[rotatedimage size];
+    
     rotatedimage=[[NSImage alloc]initWithSize:NSMakeSize(size.height, size.width)];
     [rotatedimage lockFocus];
-    //NSAffineTransform *t=[NSAffineTransform transform];
-    //[rotatedimage lockFocus];
     [t translateXBy:(image.size.width) yBy:0.0];
     [t scaleXBy:-1.0 yBy:1.0];
     
-    [t set];
-    //[t concat];
+    [t set];    
     
-    //[rotatedimage lockFocus];
+    
     [image drawAtPoint:NSMakePoint(0.0, 0.0) fromRect:NSMakeRect(0.0, 0.0, image.size.width, image.size.height) operation:NSCompositeSourceOver fraction:2.0]
     ;
-    //[t invert];
-    //[t concat];
+    
     
     
     [rotatedimage unlockFocus];
